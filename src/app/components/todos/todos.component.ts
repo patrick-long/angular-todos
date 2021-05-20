16e9d5f90c8cced27b460d023626da8f81704f70
longpatrick <long.patrick1997@gmail.com>
2021-05-20 14:33:49 -0500

    add get request to 3rd party API through service file

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from '../../Models/Todo';
+import {TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-todos',
@@ -9,27 +10,13 @@ import { Todo } from '../../Models/Todo';
 export class TodosComponent implements OnInit {
   todos!:Todo[];
 
-  constructor() {
+  constructor(private todoService:TodoService) {
   }
 
   ngOnInit(): void {
-    this.todos = [ 
-      {
-        id: 1,
-        title: 'First Todo',
-        completed: false,
-      },
-      {
-        id: 2,
-        title: 'Second Todo',
-        completed: true,
-      },
-      {
-        id: 3,
-        title: 'Third Todo',
-        completed: false,
-      },
-    ]
+    this.todoService.getTodos().subscribe(todos => {
+      this.todos = todos;
+    });
   }
 
 }
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {Observable } from 'rxjs';
+
+import { Todo } from '../Models/Todo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  todosUrl:string = 'https://jsonplaceholder.typicode.com/todos';
+  todosLimit:string = '?_limit=5';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getTodos():Observable<Todo[]> {
+    return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
+  }
 }
